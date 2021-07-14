28a5f8e08a6176c2e36c852a44a5aa8dad46bd9b
Arvin Delos Reyes <amdelosreyes@iskolarngbayan.pup.edu.ph>
2021-07-14 19:07:43 +0800

    Add category

---
+++ app/src/main/java/com/example/empr/author/AuthorHome.java
@@ -0,0 +1,20 @@
+package com.example.empr.author;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.empr.R;
+
+public class AuthorHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_author_home);
+    }
+
+    //handle click, start category add screen
+
+}
---
+++ app/src/main/java/com/example/empr/author/CategoryAddActivity.java
@@ -0,0 +1,18 @@
+package com.example.empr.author;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+
+import com.example.empr.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CategoryAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_add);
+    }
+}
